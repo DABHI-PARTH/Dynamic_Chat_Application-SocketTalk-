48a3ce931b2839b0fdd18013a077046b01709bdf
DABHI-PARTH <parthdabhi004@gmail.com>
2025-04-11 17:17:46 +0530

    Reply Chat functionality added

--- models/chatModel.js
+++ models/chatModel.js
@@ -1,22 +1,31 @@
-const mongoose= require('mongoose');
-const chatSchema= new mongoose.Schema({
-    sender_id:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User',
+const mongoose = require('mongoose');
+const chatSchema = new mongoose.Schema({
+    sender_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
     },
-    receiver_id:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User',
+    receiver_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
     },
-    message:{
-        type:String,
-        required:true
+    message: {
+        type: String,
+        required: true
+    },
+    isReply: {
+        type: Boolean,
+        default: false
+    },
+    repliedTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Chat',
+        default: null
+    },
+    repliedMessage: {
+        type: String,
+        default: ''
     }
-    
-},
-{timestamps:true}
-);
+}, { timestamps: true });
 
-
-const Chat= mongoose.model('Chat',chatSchema);
-module.exports=Chat;
+const Chat = mongoose.model('Chat', chatSchema);
+module.exports = Chat;
